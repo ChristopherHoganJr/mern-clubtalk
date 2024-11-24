5111bdc67b3e8887c56fd75bd78b1ede0584ef2c
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2024-11-24 12:32:26 -0800

    installed backend tools and modified server.js to start testing
    Installed bcrypt, cookie-parser, cors, dotenv, jsonwebtoken, mongoose, nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+
+const DB = process.env.MONGODB_URI;
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("server is working");
+  res.send(`server is listening on port:${port}`);
 });
 
 app.listen(port, () => {
-  console.log(`Server working on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
